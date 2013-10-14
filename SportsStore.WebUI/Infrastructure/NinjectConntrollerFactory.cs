76c47da627fb4ed9f1b2bd0fd0f10546f7081874
QingtingLiu <LiviaLiu742X@hotmail.com>
2013-10-14 09:45:56 +0800

    本地提交

---
+++ SportsStore.WebUI/Infrastructure/NinjectConntrollerFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectConntrollerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectConntrollerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBinds();
+        }
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBinds() { }
+    }
+}
