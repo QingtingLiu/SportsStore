53435effc0dfe97fcc41bd989ce17e072c5f3d2f
QingtingLiu <LiviaLiu742X@hotmail.com>
2013-10-15 17:40:52 +0800

    添加AddBindings

--- SportsStore.Domain/Abstract/IProductRepository.cs
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SportsStore.Domain.Entities;
+using Moq;
 
 namespace SportsStore.Domain.Abstract
 {
     public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+        }
     }
 }
